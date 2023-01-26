e9696e5cc7b6c4c28cbcbbe0f192900b85a496f3
Arin Modi <arinmodi2306@gmail.com>
2023-01-26 09:34:57 +0530

    Solved Bug

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 const { program } = require('commander')
-const { doTheBasics } = require('./commands/doTheBasics')
-const { doTheMongoSetUp } = require('./commands/mongoDB');
+const { doTheBasics } = require('../commands/doTheBasics')
+const { doTheMongoSetUp } = require('../commands/mongoDB');
 
 program.command('basic').description("Do the Basic Set Up Of API Project").action(doTheBasics);
 program.command('mongocon').description("Connect the Project With The MongoDB").action(doTheMongoSetUp);
